fd05fbd739396ec02b70fea2cb582838d282c7c2
Hector David Leiva Gamboa <hectorleivagamboa0741@gmail.com>
2026-01-10 00:36:17 -0600

    new project, ToDo cli

---
+++ src/projects/todo_cli/src/todo.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+class ToDo:
+    def __init__(self, Nombre: str, Nota: str, Fecha: datetime):
+
+        self.Nombre = Nombre
+        self.Nota = Nota
+        self.Fecha = Fecha
