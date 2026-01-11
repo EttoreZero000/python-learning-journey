d28bc14c5eb5981c9d6bac01c6295c54a8b78c4a
Hector David Leiva Gamboa <hectorleivagamboa0741@gmail.com>
2026-01-11 16:01:48 -0600

    Retomar proyecto

--- src/projects/stardew_manager/main.py
+++ src/projects/stardew_manager/main.py
@@ -1,44 +1,28 @@
-"""
-Programa principal de stardew_manager va a organizar tu granja y amistades del pueblo pelicano
+from src.lote import lote, lotes
 
-Version: 1.0.0
-Nombre: stardew_manager
+l = lotes()
 
-"""
+l.create(lote("zanahoria", 200, 3))
+l.create(lote("vino", 50, 1))
 
-"imports"
+print("=== Día 0 ===")
+print("Creciendo:", l.viewGrowing())
+print("Listos:", l.viewReady())
 
-from models.crop import Crop
-from models.farm import Farm
-from services.simulation import SimulationEngine
+l.passDay()
+print("=== Día 1 ===")
+print("Creciendo:", l.viewGrowing())
+print("Listos:", l.viewReady())
 
-try:
-    chirivia = Crop("Chirivia", 20, 4, 35)
-    granja = Farm()
+l.passDay()
+print("=== Día 2 ===")
+print("Creciendo:", l.viewGrowing())
+print("Listos:", l.viewReady())
 
-    granja.plant(chirivia, 20)
+l.passDay()
+print("=== Día 3 ===")
+print("Creciendo:", l.viewGrowing())
+print("Listos:", l.viewReady())
 
-    simulador = SimulationEngine(granja)
-
-    simulador.advance_days(5)
-
-    lista = granja.harvest_ready()
-
-    print(granja.money)
-
-    print(granja.harvest())
-
-    print(granja.money)
-
-
-    print(granja.day)
-
-    simulador.advance_days(1)
-
-    print(granja.day)
-
-
-
-except ValueError as e:
-    print(f"Error: {e}")
-
+cosecha = l.harvest(0)
+print("Cosechado:", cosecha)
--- src/projects/todo_cli/src/todo.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-class ToDo:
-    def __init__(self, Nombre: str, Nota: str, Fecha: datetime):
-
-        self.Nombre = Nombre
-        self.Nota = Nota
-        self.Fecha = Fecha
