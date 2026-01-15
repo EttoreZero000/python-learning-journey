adc36c3372f5ddd1675531c48687a31019a27619
Hector David Leiva Gamboa <hectorleivagamboa0741@gmail.com>
2026-01-14 22:32:25 -0600

    upload link in main part I

--- src/w3resource/part1/main.py
+++ src/w3resource/part1/main.py
@@ -3,6 +3,7 @@ Docstring para w3resource.part1.main
 
 This is a firt part for w3resource in python basic 1-150
 Python Basic (Part -I) - Exercises, Practice, Solution
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
 
 Name: Hector Daivd Leiva Gamboa
 Start: 01-13-2026 (MM/DD/YYYY)
@@ -19,7 +20,7 @@ def main():
         one()
     except:
         pass
-    
+
     two()
 
 if __name__ == "__main__":
