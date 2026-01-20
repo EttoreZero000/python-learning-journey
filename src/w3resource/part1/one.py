c29984fb19e40abcc8448662fd5a5615c8429530
Hector David Leiva Gamboa <hectorleivagamboa0741@gmail.com>
2026-01-20 00:07:58 -0600

    29 partI

--- src/w3resource/part1/one.py
+++ src/w3resource/part1/one.py
@@ -259,3 +259,34 @@ def list_histogram():
     plt.title('Example Histogram')
 
     plt.show()
+
+def list_to_string_concatenator():
+    list = [1, 2, "abc" ,'d', 3.141, -13]
+    newStrin = ""
+
+    for ch in list:
+        newStrin += str(ch)
+
+    return newStrin
+
+def even_numbers_until_237():
+    numbers = [    
+    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+    399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 
+    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 
+    958,743, 527
+    ]
+
+    for n in numbers:
+        print(n)
+
+        if n == 237:
+            return "I'm stop"
+        
+def unique_colors_finder():
+    #Es un conjunto
+    color_list_1 = set(["White", "Black", "Red"])
+    color_list_2 = set(["Red", "Green"])
+
+    #If use & is "interseccion"
+    return color_list_1 - color_list_2
