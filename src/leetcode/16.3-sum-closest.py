c7bfb41862b0d963140af96c7c71c04392a3177f
Hector Leiva Gamboa <hectorleivagamboa0741@gmail.com>
2026-02-11 20:45:51 -0600

    add fuction: 16.3 leetcode, with chatGTP, i don't idea.

--- src/leetcode/16.3-sum-closest.py
+++ src/leetcode/16.3-sum-closest.py
@@ -51,14 +51,34 @@
 # @lc code=start
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        size = len(nums)
-        res = set()
         nums.sort()
-
-
-##No tengo ni idea sera replicar lo anterior
-# I dont can with this problem, my biggest problem is that i use IA
+        n = len(nums)
         
+        closest = nums[0] + nums[1] + nums[2]
+        
+        for i in range(n - 2):
+            left = i + 1
+            right = n - 1
+            
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+                
+                # Si encontramos exacto, ya ganamos
+                if current_sum == target:
+                    return current_sum
+                
+                # Actualizar si es más cercano
+                if abs(current_sum - target) < abs(closest - target):
+                    closest = current_sum
+                
+                # Mover punteros
+                if current_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return closest
+
         
 # @lc code=end
 
